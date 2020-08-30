0583595d3db9d3b1a4fb6d18f18faa4b8cb2cbcc
Demilola Adefokun <d.adefokun@outlook.com>
2020-08-31 02:50:46 +0300

    created productsOverviewPage. made it the homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,25 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.indigo,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Shop App'),
-      ),
-      body: Center(),
+      home: ProductsOverviewPage(),
     );
   }
 }
