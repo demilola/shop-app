7446877aecee6b3df88b6724545df5f974b5cdf8
Demilola Adefokun <d.adefokun@outlook.com>
2020-09-02 00:13:59 +0300

    used a Consumer for necessary widgets

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -7,43 +7,46 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Now, instead of accepting data from previous pages, we use context to get the datatype whatever
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false); //The widgets using this will not listen for changes
+    //Switched to using consumer
     return ClipRRect(
-      borderRadius: BorderRadius.circular(10.0),
-      child: GridTile(
-        child: GestureDetector(
-          onTap: () => Navigator.pushNamed(
-            context,
-            ProductDetailsPage.id,
-            arguments: product.id,
+        borderRadius: BorderRadius.circular(10.0),
+        child: GridTile(
+          child: GestureDetector(
+            onTap: () => Navigator.pushNamed(
+              context,
+              ProductDetailsPage.id,
+              arguments: product.id,
+            ),
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
+          footer: GridTileBar(
+            backgroundColor: Colors.black45,
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            //Here, we need to listen for changes. Hence, we'll use a Consumer instead
+            leading: Consumer<Product>(
+      builder: (context, product, child) => IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: () => product.toggleFavoriteStatus(),
+            ),),
+            trailing: IconButton(
+              icon: Icon(
+                Icons.shopping_cart_outlined,
+                color: Theme.of(context).accentColor,
+              ),
+              onPressed: () {},
+            ),
           ),
         ),
-        footer: GridTileBar(
-          backgroundColor: Colors.black45,
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
-              color: Theme.of(context).accentColor,
-            ),
-            onPressed: () => product.toggleFavoriteStatus(),
-          ),
-          trailing: IconButton(
-            icon: Icon(
-              Icons.shopping_cart_outlined,
-              color: Theme.of(context).accentColor,
-            ),
-            onPressed: () {},
-          ),
-        ),
-      ),
     );
   }
 }
