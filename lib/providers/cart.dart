87a87e994f1c2f19eb4df044f7752cfb0f9bc907
Demilola Adefokun <d.adefokun@outlook.com>
2020-09-02 15:34:48 +0300

    created cart and cartItems

---
+++ lib/providers/cart.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items => {..._items};
+
+  void addItem(String id, String title, double price) {
+    //Check if the item is in the cart already:
+    if (_items.containsKey(id)) {
+      _items.update(
+          id,
+          (item) => CartItem(
+                id: item.id,
+                title: item.title,
+                price: item.price,
+                quantity: item.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+          id,
+          () => CartItem(
+                id: DateTime.now().toIso8601String(),
+                title: title,
+                quantity: 1,
+                price: price,
+              ));
+    }
+  }
+}
